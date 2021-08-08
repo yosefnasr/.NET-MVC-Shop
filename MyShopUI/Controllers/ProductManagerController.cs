b10a3c419c207f417915c8553e95705dac8d8aaa
Yosef Nasr <yosefnasr@protonmail.com>
2021-08-08 04:41:41 +0200

    Product Manager Pages Built and Tested

---
+++ MyShopUI/Controllers/ProductManagerController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShopMain.Models;
+using MyShopMemoryAccess;
+
+namespace MyShopUI.Controllers
+{
+    public class ProductManagerController : Controller
+    {
+        ProductRepository context;
+
+        public ProductManagerController()
+        {
+            context = new ProductRepository();
+        }
+
+        // GET: ProductManager
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            Product product = new Product();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+                return View(product);
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+                return HttpNotFound();
+            else
+                return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product oldProduct = context.Find(Id);
+            if (oldProduct == null)
+                return HttpNotFound();
+            else
+            {
+                if (!ModelState.IsValid)
+                    return View(product);
+                oldProduct.Category = product.Category;
+                oldProduct.Description = product.Description;
+                oldProduct.Image = product.Image;
+                oldProduct.Name = product.Name;
+                oldProduct.Price = product.Price;
+
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            Product product = context.Find(Id);
+
+            if (product == null)
+                return HttpNotFound();
+            else
+                return View(product);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product product = context.Find(Id);
+
+            if (product == null)
+                return HttpNotFound();
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
